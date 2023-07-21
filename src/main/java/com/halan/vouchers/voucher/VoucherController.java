64b435576aa75c6ea885092dffdb581cf4eeee47
Halan Lima <halannl85@gmail.com>
2023-07-21 19:08:04 -0300

    Commit inicial do projeto com SpringBoot, maven, java 17, swagger e autenticacao

---
+++ src/main/java/com/halan/vouchers/voucher/VoucherController.java
@@ -0,0 +1,29 @@
+package com.halan.vouchers.voucher;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class VoucherController {
+
+    @Operation(summary = "Obter todos os Vouchers", tags = { "vouchers", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "204", description = "Não existem Vouchers", content = {
+                    @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
+    @GetMapping("/vouchers")
+    List<Integer> all() {
+        return new ArrayList<>(Arrays.asList(1,2));
+    }
+}
---
+++ src/test/java/com/halan/vouchers/VouchersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halan.vouchers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouchersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
